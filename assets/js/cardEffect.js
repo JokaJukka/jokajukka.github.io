e91150f186263db1035c4d041f9b1e009580bb4b
Jarý, Miroslav <jarymiro@fel.cvut.cz>
2022-11-20 13:40:50 +0100

    🔧 Switch to DoIt theme

--- assets/js/cardEffect.js
+++ assets/js/cardEffect.js
@@ -1,13 +1,16 @@
-const cardEffectMouseHandler = e => {
-    const target = e.currentTarget;
-    const rect = target.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-
-    target.style.setProperty("--mouse-x", `${x}px`);
-    target.style.setProperty("--mouse-y", `${y}px`);
-}
-
-for (const card of document.querySelectorAll(".card")) {
-    card.addEventListener("mousemove", cardEffectMouseHandler);
+if (typeof cardEffectMouseHandler === 'undefined') {
+    const cardEffectMouseHandler = e => {
+        const target = e.currentTarget;
+        const rect = target.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+    
+        target.style.setProperty("--mouse-x", `${x}px`);
+        target.style.setProperty("--mouse-y", `${y}px`);
+    }
+    
+    
+    for (const card of document.querySelectorAll(".card")) {
+        card.addEventListener("mousemove", cardEffectMouseHandler);
+    }
 }
