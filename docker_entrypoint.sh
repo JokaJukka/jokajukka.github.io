1df3c24cf97f3333bb8d0a26e3436720ee372ffd
Ing. Miroslav Jarý <mira.jary@gmail.com>
2025-09-03 14:00:50 +0200

    ♻️ Switch & init new theme

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
-cd themes/doit
-# Check if there is something to be installed
-npm list
-if [ $? -eq 1 ]; then
-    npm install
-    npm run build
-fi
-cd ../..
-# Run HUGO
-hugo server --bind 0.0.0.0
+# see https://github.com/hugo-toha/toha
+hugo mod npm pack
+npm install
+hugo server -w --bind 0.0.0.0
